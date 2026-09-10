70393149a4a06aa6c18ea8eeb37ed13ad3ca5f94
Shankar <upaseshankar4216@gmail.com>
2026-09-10 18:14:41 +0530

    Handle API CORS preflight before database

--- fitness-backend/server.js
+++ fitness-backend/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const { app, connectDatabase } = require("./local-server");
 
 module.exports = async (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+	if (req.method === "OPTIONS") {
+		return res.status(204).end();
+	}
+
 	try {
 		await connectDatabase();
 		return app(req, res);
